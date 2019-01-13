20699b2b642ae04f41f8385a88c1be0bd148b9e2
Brendan Howell <bhowell841@gmail.com>
2019-01-13 17:07:20 -0500

    working, and mostly styled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,24 +38,27 @@ function results(){
     var correctAnswers = $("<p>").html("Correct Answers: " + correct);
     var incorrectAnswers = $("<p>").html("Incorrect Answers: " + incorrect);
     var unanswered = $("<p>").html("Unanswered: " + noAnswer);
+    var winPic = "<img src=assets/images/win.gif>";
     // create a div to display the results
-    var summary = $('<div class="col-lg-6 col-lg-offset-6 text-center" id="summary">');
+    var summary = $('<div class="col-lg-12 text-center" id="summary">');
     // append the stats to the div
     summary.append(finished);
     summary.append(correctAnswers);
     summary.append(incorrectAnswers);
     summary.append(unanswered);
+    summary.append(winPic);
     // this was confusing, took forever
     $(".row:nth(2)").append(summary);
+    
 } // close results function
 
 function timerCount(){
     timer--;
-
     // Print to timer screen
-    $("#timer").html(" " + count + " seconds");
+    $("#timer").html(" " + timer + " seconds");
 
-    if (number === 0){
+    if (timer === 0){
+        doneSound.play();
         $("#start").hide;
         hide();
         results();
@@ -63,9 +66,34 @@ function timerCount(){
     } // end if
 } // close timerCount function
 
+//Start the coundown
+function playGame() {
+    intervalId = setInterval(timerCount, 1000);
+}
+
 function end() {
     clearInterval(intervalId);
 }
 
+// hide content
+function hide() {
+    $(".form-group").hide();
+    $("#time").hide();
+    $("#done").hide();
+}
+
+// show that content
+function reveal() {
+    $(".form-group").show();
+    $("#time").show();
+    $("#done").show();
+}
+
+// compute results
+$("input[type=radio]").on("change", function(){
+    correct = $("input[value=right]:checked").length;
+    incorrect = $("input[value=wrong]:checked").length;
+    noAnswer = (11 - correct - incorrect);
+})
 
 }); //close document ready
